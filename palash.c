c7f857e21e028303090adb6517f1f9b8798d1923
Palash Mishra <palashmishra54@gmail.com>
2020-12-01 13:51:34 +0530

    Comments Added

--- palash.c
+++ palash.c
@@ -2,8 +2,8 @@
 
 int main() {
 		printf("Hello!\n");
-	char* string = "Palash Mishra";
-	printf("My enrollment number is 201B172");
+	char* string = "Palash Mishra"; // your name . \(o_o)/
+	printf("My enrollment number is 201B172");// your Enrollment number . \(o_o)/
 	printf("%s\n", string);
 return 0;
 }
